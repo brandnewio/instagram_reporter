594754c7b96fee10e51a887ade689fb14b97d14a
Kuldeep Aggarwal <kd.engineer@yahoo.co.in>
2016-09-01 20:36:41 +0530

    Remove unnecessary code

--- lib/instagram_reporter/instagram_api_caller.rb
+++ lib/instagram_reporter/instagram_api_caller.rb
@@ -168,7 +168,6 @@ class InstagramApiCaller < InstagramInteractionsBase
 
       data = remove_replacement_character(data)
                 .gsub(EMOJI_AND_SKIN_TONES_REGEXP, "")
-                .gsub(/\\+["],/, '",') # to clean up any extra back slash left
 
       # Escape special form of multibyte UTF in format \u{}
       data.gsub(/\\u{(\w+)}/, '\u\1')
